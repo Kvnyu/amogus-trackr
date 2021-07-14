beb0a0bbfe91c1a3ebb3a4d28719afbab4e29f5f
Yun Zou <yzou0008@student.monash.edu>
2021-07-14 20:25:18 +1000

    centroid and non max suppression

--- server/human_detection.py
+++ server/human_detection.py
@@ -7,6 +7,8 @@ Created on Mon Jul 12 23:03:31 2021
 
 import cv2
 import imutils
+import non_max_suppression as NMS
+import numpy as np
 
 # Window name in which image is displayed
 window_name = 'Image'
@@ -33,8 +35,7 @@ thickness = 1
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
-cap = cv2.VideoCapture(0)
-
+cap = cv2.VideoCapture('people.mp4')
 
 
 while cap.isOpened():
@@ -48,7 +49,7 @@ while cap.isOpened():
 		# pedestrians inside it
 		(regions, _) = hog.detectMultiScale(image,
 											winStride=(4, 4),
-											padding=(4, 4),
+											padding=(8, 8),
 											scale=1.05)
 
 		# Drawing the regions in the
@@ -57,10 +58,20 @@ while cap.isOpened():
 			
 		for x,y,w,h in regions:
 			cv2.rectangle(image, (x,y), (x+w,y+h), (0,255,0), 2)
+			cv2.circle(image,(x+round(w/2),y+round(h/2)), 2, (0,0,255), 2)
 			cv2.putText(image, f'person {person}', (x+10,y+20), font, 0.5, (0,0,255), 1)
 			person += 1
-	    
-			
+
+		# apply non-maxima suppression to the bounding boxes using a
+		# fairly large overlap threshold to try to maintain overlapping
+		# boxes that are still people
+		regions = np.array([[x, y, x + w, y + h] for (x, y, w, h) in regions])
+		pick = NMS.non_max_suppression(regions, overlapThresh=0.65)
+		
+		# draw the final bounding boxes
+		#for (xA, yA, xB, yB) in pick:
+		#cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 255), 2)
+	    	
 		cv2.putText(image, 'Status : Detecting ', (20,40), font , 0.6, (255,255,255), 2)
 		cv2.putText(image, f'Total Persons : {person-1}', (20,70), font , 0.6 , (255,255,255), 2)
 		
